d866095dadcd4c3f630b32265505889c08673e68
muha <1student.muhammed1@gmail.com>
2023-06-03 12:42:24 +0300

    chore : add function const + add book + diplay book

---
+++ src/main.js
@@ -0,0 +1,46 @@
+//An array to hold the books of the user 
+let myLibrary = [];
+
+function Book(title, author, pageNum, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pageNum = pageNum;
+    this.isRead = isRead
+}
+
+
+function addBookToLibrary() {
+
+    let newBook = new Book('Bad Habit', 'Muha', 23, 'no')
+    let AnewBook = new Book('Habit', 'kinaa', 223, 'yes')
+
+    myLibrary.push(newBook)
+    myLibrary.push(AnewBook)
+}
+
+function displayBook() {
+    let cardBook = document.getElementById('book-card')
+    cardBook.innerHTML = "";
+
+    let bookContainer = document.createElement('div');
+    bookContainer.classList.add('book-container');
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+
+        let bookDetails = document.createElement('div');
+        bookDetails.classList.add('book');
+
+        bookDetails.innerHTML = `     
+        <h2>${book.title}</h2>
+        <p>By: ${book.author}</p>
+        <p>Page Number: ${book.pageNum}</p>
+        <p>Read: ${book.isRead}</p>`;
+
+        bookContainer.appendChild(bookDetails);
+    }
+    cardBook.appendChild(bookContainer);
+}
+
+addBookToLibrary();
+displayBook()
